15fb3a3f03cf7da25e30a35d9d33b3c91818614b
ArnabBir <arnabbir@gmail.com>
2019-03-26 23:38:54 +0530

    working on minimax

--- assignment1.cpp
+++ assignment1.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <vector>
 
 #define ROW 6
 #define COL 7
-#define DEPTH 4
+#define MAXDEPTH 4
+#define MAXSCORE 1000000000
 
 using namespace std;
 
 bool player1;
 char grid[ROW][COL];
 int turns;
+int inpPos[COL];
 
 class Player {
 
@@ -35,6 +38,21 @@ class Player {
  			return won;
  		}
 };
+
+bool isMaxTurns() {
+	return turns >= ROW * COL;
+}
+
+vector<int>	getNextPositions() {
+
+	vector<int> v;
+	for(int i = 0; i < COL; ++i) {
+		if(inpPos[i] != -1) {
+			v.push_back(i);
+		}
+	}
+	return v;
+}
 
 void updateTurn() {
 	
@@ -77,14 +95,15 @@ void printGrid() {
 void buildGrid() {
 	
 	memset(grid, '*', sizeof grid);
+	memset(inpPos, ROW-1, sizeof inpPos);
 }
 
-bool findWin() {
-	
+bool findWin(Player p) {
+
+	char color = p.getPiece();
 	for(int j = 0; j < COL-3; ++j) {
 		for(int i = 0; i < ROW; ++i) {
-			cout<<grid[i][j]<<" "<<grid[i][j+1]<<" "<<grid[i][j+2]<<" "<<grid[i][j+3]<<endl;
-			if((grid[i][j] != '*') && (grid[i][j] == grid[i][j+1]) && (grid[i][j] == grid[i][j+2]) && (grid[i][j] == grid[i][j+3])) {
+			if((grid[i][j] == color) && (grid[i][j+1] == color) && (grid[i][j+2] == color) && (grid[i][j+3] == color)) {
 				cout<<"Horizontal : "<<i<<" "<<j<<endl;
 				return true;
 			}
@@ -93,7 +112,7 @@ bool findWin() {
 
 	for(int j = 0; j < COL; ++j) {
 		for(int i = 0; i < ROW-3; ++i) {
-			if((grid[i][j] != '*') && (grid[i][j] == grid[i+1][j]) && (grid[i][j] == grid[i+2][j]) && (grid[i][j] == grid[i+3][j])) {
+			if((grid[i][j] == color) && (grid[i+1][j] == color) && (grid[i+2][j] == color) && (grid[i+3][j] == color)) {
 				cout<<"Vertical : "<<i<<" "<<j<<endl;
 				return true;
 			}
@@ -102,7 +121,7 @@ bool findWin() {
 
 	for(int j = 0; j < COL-3; ++j) {
 		for(int i = 0; i < ROW-3; ++i) {
-			if((grid[i][j] != '*') && (grid[i][j] == grid[i+1][j+i]) && (grid[i][j] == grid[i+2][j+2]) && (grid[i][j] == grid[i+3][j+3])) {
+			if((grid[i][j] == color) && (grid[i+1][j+i] == color) && (grid[i+2][j+2] == color) && (grid[i+3][j+3] == color)) {
 				cout<<"Pos Diag : "<<i<<" "<<j<<endl;
 				return true;
 			}
@@ -111,14 +130,14 @@ bool findWin() {
 
 	for(int j = 0; j < COL-3; ++j) {
 		for(int i = 3; i < ROW; ++i) {
-			if((grid[i][j] != '*') && (grid[i][j] == grid[i-1][j+i]) && (grid[i][j] == grid[i-2][j+2]) && (grid[i][j] == grid[i-3][j+3])) {
+			if((grid[i][j] == color) && (grid[i-1][j+i] == color) && (grid[i-2][j+2] == color) && (grid[i-3][j+3] == color)) {
 				cout<<"Neg Diag : "<<i<<" "<<j<<endl;
 				return true;
 			}
 		}
 	}
 
-	return false;
+	return isMaxTurns();
 }
 
 
@@ -127,31 +146,115 @@ void play(Player p) {
 	int col, i = ROW;
 	cout<<"Please select a column (1 - 7) : ";
 	cin>>col;
-	while(i >= 0) {
-		if(grid[i][col-1] == '*') {
-			grid[i][col-1] = p.getPiece();
-			break;
-		}
-		--i;
-	}
-	if(i == -1) {
+	if(inpPos[col-1] == -1) {
 		cout<<"Invalid move ! Please try again.\n\n";
 	}
 	else {
+		grid[inpPos[col-1]][col-1] = p.getPiece();
 		updateTurn();
-		++turns;
 	}
 }
 
-void miniMax(int depth, int alpha, int beta, bool maximizingPlayer) {
-	
+int getWindowScore(int count, int empty) {
 
+	if(count == 4)	return 100;
+	if(count == 3 && empty == 1)	return 5;
+	if(count == 2 && empty == 2)	return 4;
+	if(count == 1 && empty == 3)	return 3;
+	if(count == 3 && empty == 0)	return -1;
+	if(count == 2 && empty == 1)	return -1;
+	if(count == 2 && empty == 0)	return -3;
+	if(count == 1 && empty == 2)	return -3;
+	if(count == 1 && empty == 1)	return -4;
+	return 0;
 }
 
-void playComp(Player p) {
-	cout<<player1<<" ";
-	updateTurn();
-	cout<<player1<<endl;
+int getScore(player p) {
+
+	int score = 0, countP, empty;
+	char color = p.getPiece();
+	for(int i = 0; i < ROW; ++i) {
+		if(grid[i][COL/2] == color) {
+			score += 3;
+		}
+	}
+	for(int i = 0; i < ROW; ++i) {
+		for(int j = 0; j < COL-3; ++j) {
+			countP = 0, empty = 0;
+			for(int k = 0; k < 3; ++k) {
+				if(grid[i][j+k] == color) {
+					++countP;
+				}
+				else if(grid[i][j+k] == '*') {
+					++empty;
+				}
+			}
+			score += getWindowScore(countP, empty);
+		}
+	}
+	for(int j = 0; j < COL; ++j) {
+		for(int i = 0; i < ROW-3; ++i) {
+			countP = 0, empty = 0;
+			for(int k = 0; k < 3; ++k) {
+				if(grid[i+k][j] == color) {
+					++countP;
+				}
+				else if(grid[i+k][j] == '*') {
+					++empty;
+				}
+			}
+			score += getWindowScore(countP, empty);
+		}
+	}
+	for(int i = 0; i < ROW-3; ++i) {
+		for(int j = 0; j < COL-3; ++j) {
+			countP = 0, empty = 0;
+			for(int k = 0; k < 3; ++k) {
+				if(grid[i+k][j+k] == color) {
+					++countP;
+				}
+				else if(grid[i+k][j+k] == '*') {
+					++empty;
+				}
+			}
+			score += getWindowScore(countP, empty);
+		}
+	}
+	for(int i = 0; i < ROW-3; ++i) {
+		for(int j = 3; j < COL; ++j) {
+			countP = 0, empty = 0;
+			for(int k = 0; k < 3; ++k) {
+				if(grid[i+k][j-k] == color) {
+					++countP;
+				}
+				else if(grid[i+k][j-k] == '*') {
+					++empty;
+				}
+			}
+			score += getWindowScore(countP, empty);
+		}
+	}
+	return score;
+}
+
+int miniMax(Player p1, Player p2, int depth, int alpha, int beta, bool maximizingPlayer) {
+	
+	vector<int> nextPos = getNextPositions(p1);
+	bool winP1 = findWin(p1);
+	bool winP2 = findWin(p2);
+	if(winP1)	return -MAXSCORE;
+	if(winP2)	return MAXSCORE;
+	if(isMaxTurns())	return 0;
+	if(depth == 0) {
+		return getScore(p2);
+	}
+	if(maximizingPlayer) {
+		
+	}
+}
+
+void playComp(Player p, bool realTurn) {
+	if(realTurn)	updateTurn();
 }
 
 int main() {
@@ -166,7 +269,7 @@ int main() {
 			play(user);
 		}
 		else {
-			playComp(comp);
+			playComp(comp, true);
 		}
 		printGrid();
 	}while(!findWin());
